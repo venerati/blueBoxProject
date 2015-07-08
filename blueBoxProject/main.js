f9bce334fa784582e83ca9f015a19e75acbd2ff9
venerati <jamesomarshall@gmail.com>
2015-07-07 20:37:32 -0400

    Js is set up so that desired action takes place

---
+++ blueBoxProject/main.js
@@ -0,0 +1,13 @@
+	
+	//var box = document.getElementById('bluebox');
+
+function toggle() {
+	var text = document.getElementById('text');
+
+	if (text.className === 'class1') {
+		text.className = 'class2';
+	} else {
+		text.className = 'class1';
+	}
+
+};
